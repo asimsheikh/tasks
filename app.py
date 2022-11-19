9363629d7366934383d66aedf5fef6153005139a
asheikh <asim.sardar.sheikh@gmail.com>
2022-11-20 10:34:39 +1100

    getting task data for post request

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 from typing import Optional
 
 from pydantic import BaseModel
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 class Task(BaseModel):
     id: Optional[str] = uuid4().hex
@@ -15,3 +15,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api', methods=['POST'])
+def api():
+    if request.method == 'POST':
+        return jsonify(request.form) 
+    return 'api route'
