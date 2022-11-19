30eb127057d5dadff9ffc35cbe73d9557821000a
asheikh <asim.sardar.sheikh@gmail.com>
2022-11-20 10:12:34 +1100

    adding task model and styling task form

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+from uuid import uuid4
+from typing import Optional
+
+from pydantic import BaseModel
 from flask import Flask, render_template
+
+class Task(BaseModel):
+    id: Optional[str] = uuid4().hex
+    name: str 
+    completed: bool
+    pomodoros: int
 
 app = Flask(__name__)
 
