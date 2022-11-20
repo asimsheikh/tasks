23fe9d78a5a305399908b23cb42b2bba7eb7299a
asheikh <asim.sardar.sheikh@gmail.com>
2022-11-21 10:38:43 +1100

    Adding the Notes type to write notes against a task

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel, Field
 
 from uuid import uuid4
@@ -7,3 +8,10 @@ class Task(BaseModel):
     name: str 
     completed: bool = False
     pomodoros: int = 0
+
+
+class Notes(BaseModel):
+    id: str = Field(default_factory=lambda: uuid4().hex)
+    date: datetime = Field(default_factory=datetime.now)
+    text: str
+    task_id: str 
