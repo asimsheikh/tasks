faaccd7a89cad3d73cf4362ce14321196d4aa086
asheikh <asim.sardar.sheikh@gmail.com>
2022-11-21 13:21:50 +1100

    adding a to_note serialize function

--- serialize.py
+++ serialize.py
@@ -1,4 +1,4 @@
-from models import Task
+from models import Task, Notes
 
 def to_task(form_data: dict[str,str]) -> Task:
     _id = form_data.get('id') 
@@ -6,3 +6,15 @@ def to_task(form_data: dict[str,str]) -> Task:
     completed = form_data.get('task_completed')
     pomodoros = form_data.get('task_pomodoros')
     return Task(id=_id, name=name, completed=bool(completed), pomodoros=int(pomodoros))
+
+def to_note(form_data: dict[str, str]) -> Notes:
+    _id = form_data.get('id')
+    date = form_data.get('notes_date')
+    text = form_data.get('notes_text')
+    task_id = form_data.get('notes_task_id')
+    if _id:
+        return Notes(id=_id, date=date, text=text, task_id=task_id)
+    else:
+        return Notes(date=date, text=text, task_id=task_id)
+
+    
