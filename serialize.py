d01701f2c09e2a8c9683e995fed20f1d31461fd0
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-08 21:31:48 +1100

    adding to_contact serializer

--- serialize.py
+++ serialize.py
@@ -1,4 +1,4 @@
-from models import Task, Notes 
+from models import Task, Notes, Contact
 
 def to_task(form_data: dict[str,str]) -> Task:
     _id = form_data.get('id') or '' 
@@ -17,4 +17,13 @@ def to_note(form_data: dict[str, str]) -> Notes:
     else:
         return Notes(date=date, text=text, task_id=task_id)
 
-    
+def to_contact(form_data: dict[str, str]) -> Contact:
+    _id = form_data.get('contact_id')
+    first_name = form_data.get('contact_first_name') or ''
+    last_name = form_data.get('contact_last_name') or ''
+    email = form_data.get('contact_email') or ''
+    if _id:
+        return Contact(id=_id, first_name=first_name, last_name=last_name, email=email)
+    else:
+
+        return Contact(first_name=first_name, last_name=last_name, email=email)
