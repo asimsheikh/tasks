81260b403e7a0184110ed218077676d9217a06bd
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-12 13:27:29 +0700

    Removing old code

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, redirect, render_template_string
+from flask import Flask, request, redirect, render_template_string
 
 from persist import Persist
 from models import Task
-from serialize import to_task, to_note, to_contact
 
 db = Persist()
 app = Flask(__name__)
@@ -14,14 +13,28 @@ def temp_clear():
 
 @app.route('/')
 def index():
-    if db.get_all().get('tasks'):
-        return render_template('index.html', data={'tasks': db.get('tasks')})
-    else:
-        return render_template('index.html', data={'tasks': []})
+    return 'In the tasks app'
 
-@app.route('/api', methods=['POST'])
+@app.route('/api', methods=['POST', 'GET'])
 def api():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        page = '''
+        {% extends "base.html" %}
+        {% block content %}
+            <pre class='text-gray-400 pl-40'>{{data | pprint}}</pre>
+            <div>
+                <p><a class="font-extrabold pb-2" href={{url_for('index')}}>Home</a></p>
+                {% for task in data.tasks %}
+                    <p>{{task.name}}</p>
+                {% endfor %}
+            </div>
+        {% endblock %}
+        '''
+        db_all = db.get_all()
+        data = {'tasks': db_all.get('tasks', [])}
+        return render_template_string(page, data=data)
+
+    elif request.method == 'POST':
         if request.form['action'] == 'add_task':
             task = Task(name=request.form.get('task_name'))
             db.add(key='tasks', data=task.dict())
@@ -30,15 +43,8 @@ def api():
             task = to_task(request.form)
             db.update(key='tasks', item_id=task.id, data=task.dict())
             return redirect('/')
-        elif request.form['action'] == 'add_notes':
-            note = to_note(request.form)
-            db.add(key='notes', data=note.dict())
-            return redirect('/notes/' + request.form['notes_task_id'])
-        elif request.form['action'] == 'save_note':
-            note = to_note(request.form)
-            db.update(key='notes', item_id=note.id, data=note.dict())
-            return redirect('/notes/' + request.form['notes_task_id'])
-    return 'api route'
+
+    return 'In api routes'
 
 @app.route('/contacts/<string:id>', methods=['GET', 'POST', 'PUT'])
 def contacts(id: str):
@@ -85,22 +91,4 @@ def contacts(id: str):
     '''
     contacts = db.get(key='contacts')
     data = {'contact': contacts[0]}
-    return render_template_string(page, data=data)
-
-@app.route('/tasks')
-def tasks():
-    page = '''
-    {% extends "base.html" %}
-    {% block content %}
-        <pre class='text-gray-400 pl-40'>{{data | pprint}}</pre>
-        <div>
-            <p><a class="font-extrabold pb-2" href={{url_for('index')}}>Home</a></p>
-            {% for task in data.tasks %}
-                <p>{{task.name}}</p>
-            {% endfor %}
-        </div>
-    {% endblock %}
-    '''
-    db_all = db.get_all()
-    data = {'tasks': db_all['tasks']}
     return render_template_string(page, data=data)
