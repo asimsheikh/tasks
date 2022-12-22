187a23e06fd02b39bcacba80355ba6b30f40129d
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-22 08:26:04 +0700

    Implementing toggled pebbles

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template_string
-
 from persist import Persist
 
 db = Persist()
@@ -13,15 +12,18 @@ def temp_clear():
 @app.route('/pebble',  methods=['POST'])
 def pebble():
 	print(request.form)
-	data = dict(pebble_id=int(request.form['pebble_id']), id=request.form['id'])
+	# data = dict(pebble_id=int(request.form['pebble_id']), id=request.form['id'])
+	pebble = [ pebble for pebble in db.get('pebbles') if pebble['id'] == request.form['id'] ][0]
+	pebble = { **pebble,**{"checked": not pebble['checked']}}
+	db.update('pebbles', request.form['id'], pebble)
 	page='''
-        <div id="{{ data.pebble_id }}" class="grow mx-2 bg-red-300 w-14"
+        <div id="{{ data.pebble_id }}" class="grow mx-2 bg-{{ 'green' if data.checked else 'red' }}-300 w-14"
             hx-post="/pebble" hx-swap="outerHTML" 
             hx-vals='{"pebble_id": {{ data.pebble_id}}, "id": "{{ data.id }}" }'>
-            {{ data.pebble_id }} {{ data.id }} 
+            {{ data.pebble_id }} 
         </div>
 	'''
-	return render_template_string(page, data=data)
+	return render_template_string(page, data=pebble)
 
 @app.route('/')
 def index():
@@ -33,7 +35,7 @@ def index():
         </div>
         <div id="pebbles" class="flex flex-row">
           {% for pebble in data.pebbles %}
-            <div id="{{ pebble.pebble_id }}" class="grow mx-2 bg-green-300 w-14"
+            <div id="{{ pebble.pebble_id }}" class="grow mx-2 bg-{{ 'green' if pebble.checked else 'red' }}-300 w-14"
                 hx-post="/pebble" hx-swap="outerHTML" 
                 hx-vals='{"pebble_id": {{ pebble.pebble_id}}, "id": "{{ pebble.id }}" }'>
                 {{ pebble.pebble_id }} 
