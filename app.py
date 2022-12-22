4a44fd4022cb45fa835f844e86429ad04d155b25
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-22 09:18:22 +0700

    Going back to templates as opposed to inline strings, due to highlighting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template
 from persist import Persist
 
 db = Persist()
@@ -14,34 +14,10 @@ def pebble():
 	pebble = [ pebble for pebble in db.get('pebbles') if pebble['id'] == request.form['id'] ][0]
 	pebble = { **pebble,**{"checked": not pebble['checked']}}
 	db.update('pebbles', request.form['id'], pebble)
-	page='''
-        <div id="{{ data.pebble_id }}" class="grow mx-2 bg-{{ 'green' if data.checked else 'red' }}-300 w-14"
-            hx-post="/pebble" hx-swap="outerHTML" 
-            hx-vals='{"pebble_id": {{ data.pebble_id}}, "id": "{{ data.id }}" }'>
-            {{ data.pebble_id }} 
-        </div>
-	'''
-	return render_template_string(page, data=pebble)
+	pebbles = db.get('pebbles')
+	return render_template('pebbles.html', data={"pebbles": pebbles})
 
 @app.route('/')
 def index():
-    page = '''
-    {% extends "base.html" %}
-    {% block content %}
-        <div class="mb-4">
-          <p class="text-xl font-extrabold">Pebbles App</p>
-		  <p>Another Change</p>
-        </div>
-        <div id="pebbles" class="flex flex-row">
-          {% for pebble in data.pebbles %}
-            <div id="{{ pebble.pebble_id }}" class="grow mx-2 bg-{{ 'green' if pebble.checked else 'red' }}-300 w-14"
-                hx-post="/pebble" hx-swap="outerHTML" 
-                hx-vals='{"pebble_id": {{ pebble.pebble_id}}, "id": "{{ pebble.id }}" }'>
-                {{ pebble.pebble_id }} 
-            </div>
-          {% endfor %}
-        <div>
-    {% endblock %}
-    '''
     pebbles = db.get('pebbles')
-    return render_template_string(page, data={"pebbles": pebbles})
+    return render_template('pebbles.html', data={"pebbles": pebbles})
