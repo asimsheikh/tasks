5c522cad843a1d0fe24fb5c335ac3b39d8dc2855
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-22 09:01:42 +0700

    Attempted livereload, didnt work, remove log statements

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ def temp_clear():
 
 @app.route('/pebble',  methods=['POST'])
 def pebble():
-	print(request.form)
-	# data = dict(pebble_id=int(request.form['pebble_id']), id=request.form['id'])
 	pebble = [ pebble for pebble in db.get('pebbles') if pebble['id'] == request.form['id'] ][0]
 	pebble = { **pebble,**{"checked": not pebble['checked']}}
 	db.update('pebbles', request.form['id'], pebble)
@@ -32,6 +30,7 @@ def index():
     {% block content %}
         <div class="mb-4">
           <p class="text-xl font-extrabold">Pebbles App</p>
+		  <p>Another Change</p>
         </div>
         <div id="pebbles" class="flex flex-row">
           {% for pebble in data.pebbles %}
