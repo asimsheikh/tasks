e51ca218b731e52e903717a93a16d989d3b9d1f0
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-31 20:35:39 +0700

    removing unused testing route

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, request, render_template_string
 from persist import Persist
-from models import Task
-
-from htm import div, p, form, field, button
 
 db = Persist()
 app = Flask(__name__)
-repo = dict(username='asimsheikh', password='bestintheworld')
 
 HEAD = '''  
     <head>
@@ -17,66 +13,6 @@ HEAD = '''
         <script src="{{url_for('static', filename='htmx.min.js')}}"></script>
     </head>
 ''' 
-
-@app.route('/testing', defaults={'task_id': None})
-@app.route('/testing/<task_id>', methods=['GET','POST', 'PUT'])
-def testing(task_id: str):
-    if request.method == 'POST' and task_id:
-        task = [ task for task in db.get('tasks') if task['id'] == task_id ][0]
-        task = Task(**task)
-        return f'''
-            <form hx-put="/testing/{task.id}" hx-target="div[id='{task.id}']" hx-swap="outerHTML">
-                <div>
-                    <label class="font-bold">Task Name</label>
-                    <input autofocus onfocus="this.select()" class="w-3/4" type="text" name="task_name" value="{task.name}" placeholder="wash the dishes">
-                </div>
-                <button class="rounded-md mt-2 border-2 border-zinc-700 p-2 focus:outline-none">Submit</button>
-                <button class="rounded-md mt-2 border-2 border-zinc-700 p-2 focus:outline-none" hx-get="/testing/{task.id}">Cancel</button>
-            </form> 
-        '''
-
-    if request.method == 'GET' and task_id:
-        task = [ task for task in db.get('tasks') if task['id'] == task_id ][0]
-        task = Task(**task)
-        ps = div(
-               p(task.name, class_='py-2 text-neutral-500'), 
-               p(task.id or '', class_="font-bold text-neutral-700"), 
-               class_='p-4 mx-6 my-2 border', id=task.id, hx_post=f'/testing/{task.id}', hx_trigger='dblclick', hx_target=f"div[id='{task.id}']",
-               hx_swap='innerHTML'
-            )
-        return ps
-
-    if request.method == 'PUT' and task_id:
-        task = [ task for task in db.get('tasks') if task['id'] == task_id ][0]
-        name = request.form['task_name']
-        task = Task(**task)
-        task.name = name
-        db.update('tasks', item_id=task_id,data=task.dict())
-        ps = div(
-               p(task.name, class_='py-2 text-neutral-500'), 
-               p(task.id or '', class_="font-bold text-neutral-700"), 
-               class_='p-4 mx-6 my-2 border hover:bg-neutral-200', id=task.id, hx_post=f'/testing/{task.id}', hx_trigger='dblclick', hx_target=f"div[id='{task.id}']",
-               hx_swap='innerHTML'
-            )
-        return ps
-
-    tasks: list[str] = []
-    for db_task in db.get('tasks'):
-        task = Task(**db_task)
-        ps = div(
-               p(task.name, class_='py-2 text-neutral-500'), 
-               p(task.id or '', class_="font-bold text-neutral-700"), 
-               class_='p-4 mx-6 my-2 border hover:bg-neutral-100',
-               id=task.id,
-               hx_post=f'/testing/{task.id}',
-               hx_trigger='dblclick',
-               hx_target='this',
-               hx_swap=f'#{task.id}'
-            )
-        tasks.append(ps)
-        
-    container = div(*tasks, id='pebbles', class_='flex flex-col grow m-2', hx_post='/pebbles', hx_target='#pebbles')
-    return render_template_string(HEAD + container, data={})
 
 @app.post('/change/<path:subpath>')
 def change(subpath: str):
@@ -89,13 +25,17 @@ def change(subpath: str):
     elif subpath == 'username/add':
         username = request.form['username']
         repo['username'] = username
-        page = ''' 
+        page = '''
+        <!-- html -->
+        
             <div class="border-zinc-700 focus:outline-none"
                 id="username"
                 hx-post="/change/username"
                 hx-trigger="click"
                 hx-target="#username"
                 hx-swap="outerHTML">Username: {{ data.username }}</div>
+
+        <!-- !html -->     
         '''
         return render_template_string(HEAD + page, data=repo)
     else:
