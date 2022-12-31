7c3c757c841aac5b3d1107cab6fd506029a3410b
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-31 22:05:26 +0700

    created updating legal name functionality

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, request, render_template_string
 from persist import Persist
 
+class Repo:
+    def __init__(self): 
+        self.data = dict(first_name="Asim", last_name="Sheikh")
+    
+    def get_name(self):
+        return self.data
+
+    def update_name(self, first_name: str, last_name: str):
+        self.data = dict(first_name=first_name, last_name=last_name)
+
 db = Persist()
 app = Flask(__name__)
+repo = Repo()
 
 HEAD = '''  
     <head>
@@ -16,28 +27,59 @@ HEAD = '''
 
 @app.post('/change/<path:subpath>')
 def change(subpath: str):
-    if subpath == 'username':
+    if subpath == 'legal_name':
         return render_template_string(''' <!-- html -->
-                    <form hx-post="/change/username/add">
-                        <input type="text" value="{{data.username}}" name="username" id="username" autofocus onfocus="this.select()"/>
-                    </form>
-                <!-- !html --> ''', data=repo)
-    elif subpath == 'username/add':
-        username = request.form['username']
-        repo['username'] = username
+        <div id="legal_name">
+            <div class="m-4">
+                <p class="text-lg">Legal name</p>
+                <p class="text-sm">This is the name on your travel document, which could be a licence or a passport.</p>
+            </div>
+            <form class="m-2 flex flex-col" 
+                  hx-post="/change/legal_name/add"
+                   hx-target="#legal_name"
+                   hx-swap="outerHTML">
+                <div class="flex flex-row">
+                    <div class="flex flex-col border-2 rounded-md w-4/12 mx-2 p-2">
+                        <label class="text-sm" for="first_name">First name</label>
+                        <input type="text" value="{{data.first_name}}" name="first_name" id="first_name" autofocus onfocus="this.select()"/>
+                    </div>
+                    
+                    <div class="flex flex-col border-2 rounded-md w-4/12 mx-2 p-2">
+                        <label class="text-sm" for="last_name">Surname</label>
+                        <input type="text" value="{{data.last_name}}" name="last_name" id="last_name" />
+                    </div>
+                </div>
+                    <div class="bg-black text-white px-4 m-2 py-2 w-1/12 rounded-md">
+                        <input type="submit" value="Save" />
+                    </div>
+            </form>
+        </div>
+        <!-- !html --> ''', data=repo.get_name())
+
+    elif subpath == 'legal_name/add':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        repo.update_name(first_name=first_name, last_name=last_name )
         page = '''
         <!-- html -->
         
-            <div class="border-zinc-700 focus:outline-none"
-                id="username"
-                hx-post="/change/username"
-                hx-trigger="click"
-                hx-target="#username"
-                hx-swap="outerHTML">Username: {{ data.username }}</div>
+            <div id="legal_name" class="flex">
+                <div >
+                    <p class="text-lg font-semibold">Legal name</p>
+                    <p>{{data.first_name}} {{data.last_name}}</p>
+                </div>
+
+              <button class="ml-2 border-zinc-700 border-2 px-2 rounded-md"
+                   id="password"
+                   hx-post="/change/legal_name"
+                   hx-trigger="click"
+                   hx-target="#legal_name"
+                   hx-swap="outerHTML">Edit</button>
+            </div>
 
         <!-- !html -->     
         '''
-        return render_template_string(HEAD + page, data=repo)
+        return render_template_string(HEAD + page, data=repo.get_name())
     else:
         return '<div>nothing...</div>'
 
@@ -46,22 +88,20 @@ def index():
     page = '''
        <!-- html -->
         <body>
-            <div>
-              <div class="border-zinc-700 focus:outline-none"
-                   id="username"
-                   hx-post="/change/username"
-                   hx-trigger="click"
-                   hx-target="#username"
-                   hx-swap="outerHTML">Username: {{ data.username }}</div>
+            <div id="legal_name" class="flex">
+                <div >
+                    <p class="text-lg font-semibold">Legal name</p>
+                    <p>{{data.first_name}} {{data.last_name}}</p>
+                </div>
 
-              <div class="border-zinc-700 focus:outline-none"
+              <button class="ml-2 border-zinc-700 border-2 px-2 rounded-md"
                    id="password"
-                   hx-post="/change"
+                   hx-post="/change/legal_name"
                    hx-trigger="click"
-                   hx-target="#change"
-                   hx-swap="outerHTML">Password: {{ data.password }}</div>
+                   hx-target="#legal_name"
+                   hx-swap="outerHTML">Edit</button>
             </div>
         </body>
        <!-- !html -->     
     '''
-    return render_template_string(HEAD + page, data=repo)
+    return render_template_string(HEAD + page, data=repo.get_name())
